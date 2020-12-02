100c794b5cebb8af7783ded90775409e54b507ea
Jay Asbury <vbjay.net@gmail.com>
2020-12-02 02:51:18 -0500

    Lock down Privacy page

--- src/mvc/Controllers/HomeController.cs
+++ src/mvc/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -26,6 +27,7 @@ namespace mvc.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
