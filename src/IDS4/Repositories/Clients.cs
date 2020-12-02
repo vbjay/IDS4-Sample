50de6961e426c7b45397f164b0f562f8d702f763
Jay Asbury <vbjay.net@gmail.com>
2020-12-02 00:50:57 -0500

    Adjust client to allow both scopes

--- src/IDS4/Repositories/Clients.cs
+++ src/IDS4/Repositories/Clients.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+
 using IdentityServer4.Models;
 
 internal class Clients
@@ -11,7 +12,7 @@ internal class Clients
                 ClientName = "Example client application using client credentials",
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
-                AllowedScopes = new List<string> {"api1.read"}
+                AllowedScopes = new List<string> {"api1.write", "api1.read" }
             }
         };
 }
