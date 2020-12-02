de6e567d30571d0dada60dd871405056ddcbcf0d
Jay Asbury <vbjay.net@gmail.com>
2020-12-02 02:42:40 -0500

    Add oidc authentication

--- src/mvc/Startup.cs
+++ src/mvc/Startup.cs
@@ -27,8 +27,23 @@ namespace mvc
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
             })
-                .AddCookie("cookie");
+                .AddCookie("cookie")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = "https://localhost:5001";//ids4 url
+                    options.ClientId = "oidcClient";
+                    options.ClientSecret = "SuperSecretPassword";
+                    options.ResponseType = "code";
+                    options.UsePkce = true;
+                    options.ResponseMode = "query";
+                    // options.CallbackPath = "/signin-oidc"; // default redirect URI
+                    // options.Scope.Add("oidc"); // default scope
+                    // options.Scope.Add("profile"); // default scope
+                    options.Scope.Add("api1.read");
+                    options.SaveTokens = true;
+                });
 
             services.AddControllersWithViews();
         }
@@ -46,19 +61,15 @@ namespace mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
         }
     }
 }
