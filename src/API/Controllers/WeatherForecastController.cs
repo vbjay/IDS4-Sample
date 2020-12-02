241e26c3b06af02eac69ef9a97dae3e79945c3ae
Jay Asbury <vbjay.net@gmail.com>
2020-12-02 01:48:29 -0500

    Default to application/json

--- src/API/Controllers/WeatherForecastController.cs
+++ src/API/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace api.Controllers
     [ApiController]
     [Route("[controller]")]
     [Authorize]
+    [Produces("application/json")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
