34eda08342689e11e879806928b7532c8883b98d
Jay Asbury <vbjay.net@gmail.com>
2020-12-01 21:08:27 -0500

    Add missing namespaces

--- src/IDS4/Startup.cs
+++ src/IDS4/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
