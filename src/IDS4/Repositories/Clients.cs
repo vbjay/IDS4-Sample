c439f0c7335a446f215b2f1309fb3a34bf78219a
Jay Asbury <vbjay.net@gmail.com>
2020-12-02 02:12:48 -0500

    Add oidc client

--- src/IDS4/Repositories/Clients.cs
+++ src/IDS4/Repositories/Clients.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 
+using IdentityServer4;
 using IdentityServer4.Models;
 
 internal class Clients
@@ -13,6 +14,24 @@ internal class Clients
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
                 AllowedScopes = new List<string> {"api1.write", "api1.read" }
+            },
+            new Client
+            {
+                ClientId = "oidcClient",
+                ClientName = "Example Client Application",
+                ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, // change me!
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = new List<string> {"https://localhost:44386/signin-oidc"},
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "role",
+                    "api1.read"
+                },
+                RequirePkce = true,
+                AllowPlainTextPkce = false
             }
         };
 }
